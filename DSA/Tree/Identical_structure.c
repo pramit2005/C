014b3107a3f68764f7f2568ad5d547051b6f6156
Pramit Sarkar <pramitsarkat@gmail.com>
2025-11-16 20:28:05 +0530

    .

---
+++ DSA/Tree/Identical_structure.c
@@ -0,0 +1,180 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<limits.h>
+typedef struct node{
+    struct node *left;
+    struct node *right;
+    int data;
+}Tree;
+typedef struct queue_node{
+    struct queue_node*next;
+    Tree *ptr;
+}QNode;
+typedef struct Q{
+    QNode *front;
+    QNode *rear;
+}Queue;
+QNode* createNode(Tree *p){
+    QNode *Q=(QNode*)malloc(sizeof(QNode));
+    if(Q==NULL)
+        return NULL;
+    Q->ptr=p;
+    Q->next=NULL;
+    return Q;
+}
+void Enqueue(Queue *q,Tree *p){
+    QNode *newnode=createNode(p);
+    if(newnode==NULL){
+        printf("\n Memory allocation failed");
+        return;
+    }
+    if(q->rear==NULL)
+        q->rear=q->front=newnode;
+    else{
+        q->rear->next=newnode;
+        q->rear=newnode;
+    }
+}
+Tree* Dequeue(Queue *q){
+    if(q->rear==NULL){
+        printf("\n Queue is empty");
+        return NULL;
+    }
+    else if(q->front->next==NULL){
+        Tree *temp_ptr=q->front->ptr;
+        QNode *temp=q->front;
+        q->front=NULL;
+        q->rear=NULL;
+        free(temp);
+        return temp_ptr;
+    }
+    else{
+        Tree *temp_ptr=q->front->ptr;
+        QNode *temp=q->front;
+        q->front=q->front->next;
+        temp->next=NULL;
+        free(temp);
+        return temp_ptr;
+    }
+}
+int IsEmpty(Queue *q){
+    return (q->rear==NULL);
+}
+void Delete_Queue(Queue *q){
+    if(q->rear==NULL)
+        return;
+    QNode *current=q->front;
+    QNode *next_node;
+    while(current){
+        next_node=current->next;
+        free(current);
+        current=next_node;
+    }
+    q->front=NULL;
+    q->rear=NULL;
+}
+void Insert(Tree **root_ptr,int x){ //level order traversal
+    Queue Q={NULL,NULL};
+    Tree *temp;
+    Tree *newnode=(Tree*)malloc(sizeof(Tree));
+    newnode->data=x;
+    newnode->left=newnode->right=NULL;
+    Tree *root=*root_ptr;
+    if(root==NULL){
+    *root_ptr=newnode;
+    return;
+    }
+    Enqueue(&Q,root);
+    while(!IsEmpty(&Q)){
+        temp=Dequeue(&Q);
+        if(temp->left){
+            Enqueue(&Q,temp->left);
+        }
+        else{
+            temp->left=newnode;
+            Delete_Queue(&Q);
+            return;
+        }
+        if(temp->right){
+            Enqueue(&Q,temp->right);
+        }
+        else{
+            temp->right=newnode;
+            Delete_Queue(&Q);
+            return;
+        }
+    }
+    Delete_Queue(&Q);
+}
+void display(Tree *root){ //level order traversal
+    Tree *temp;
+    if(root==NULL){
+        printf("\n The tree is empty");
+        return;
+    }
+    Queue q={NULL,NULL};
+    Enqueue(&q,root);
+    while(!IsEmpty(&q)){
+        temp=Dequeue(&q);
+        printf(" %d ",temp->data);
+        if(temp->left)
+            Enqueue(&q,temp->left);
+        if(temp->right)
+            Enqueue(&q,temp->right);
+    }
+    Delete_Queue(&q);
+}
+int Identical_struct(Tree *root1,Tree *root2){
+    if(root1==NULL && root2==NULL)
+        return 1;
+    if(root1==NULL || root2==NULL)
+        return 0;
+    else
+        return ((root1->data==root2->data) && Identical_struct(root1->left,root2->left) && Identical_struct(root1->right,root2->right));
+}
+Tree *root1=NULL,*root2=NULL;
+int main(){
+    int a,b,t,x;
+    Tree *deepest;
+    while(1){
+        printf("\n 1.Insert\n 2.Display(Level order)\n 3.Are the trees identical?\n 5.Exit");
+        printf("\n Enter a option: ");
+        scanf("%d",&a);
+        switch(a){
+            case 1:
+                printf("\n 1.Tree 1\n 2.Tree 2");
+                printf("\n Enter a option: ");
+                scanf("%d",&t);
+                printf("\n Enter the data: ");
+                scanf("%d",&b);
+                if(t==1)
+                Insert(&root1,b);
+                if(t==2)
+                Insert(&root2,b);
+                break;
+            case 2:
+                printf("\n 1.Tree 1\n 2.Tree 2");
+                printf("\n Enter a option: ");
+                scanf("%d",&t);
+                if(t==1)
+                display(root1);
+                if(t==2)
+                display(root2);
+                break;
+            case 3:
+                x=Identical_struct(root1,root2);
+                if(x==1)
+                    printf("\n Yes,trees are identical");
+                else
+                    printf("\n No,trees are not identical");
+                break;
+            case 5:
+                exit(0);
+                break;
+            default:
+                printf("\n Invalid opion");
+                break;
+        }
+    }
+    return 0;
+}
--- test3.c
+++ test3.c
@@ -1,17 +1,29 @@
 #include<stdio.h>
-float area()
-{
-    int r;
-    float pie=3.14;
-    printf("enter the radius:");
-    scanf("%d",&r);
-    float a=pie*(r*r);
-    return a;
-}
 int main()
 {
-    float b=0;
-    b=area();
-    printf("%d",b);
-    return 0;
+    char a;
+    printf("enter a char: ");
+    scanf("%c",&a);
+    switch(a)
+    {
+        case 'a':
+            printf("\n %c is a vowel",a);
+            break;
+        case 'e':
+            printf("\n %c is a vowel",a);
+            break;
+        case 'i':
+            printf("\n %c is a vowel",a);
+            break;
+        case 'o':
+            printf("\n %c is a vowel",a);
+            break;
+        case 'u':
+            printf("\n %c is a vowel",a);
+            break;
+        default:
+            printf("%c is a consonant",a);
+            break;
+    }
+return 0;
 }
